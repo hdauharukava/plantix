d877cb93a5610666ff0cecb4eadc40f31434c71f
Polina Bezukladnova <bezukladnovap@gmail.com>
2025-11-15 17:09:48 +0100

    typo

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
-    <Navbar />
+    <NavBar />
     <NuxtPage />
     <Footer />
   </div>
 </template>
+
+<script setup>
+import NavBar from './components/NavBar.vue';
+import Footer from './components/Footer.vue';
+</script>
